3d2d892df472519b6a4c948ce3f9302d57aa9212
carecoding <118536562+carecoding@users.noreply.github.com>
2022-12-01 14:46:10 +0000

    测试跨域。

---
+++ vuetify-ex1/vuetify-test/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- <HelloWorld /> -->
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-btn block color="primary">Hello</v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="8">
+            <v-btn block color="success">World 1</v-btn>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-btn block color="success">World 2</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
